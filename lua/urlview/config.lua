ce8977f9119b8066651a28d8d4a483f5585deb7b
Andrew <x.and.i.rew.e@gmail.com>
2022-04-01 06:51:58 +1100

    feat: can search packer plugin links (closes #2), added back default_picker setting

--- lua/urlview/config.lua
+++ lua/urlview/config.lua
@@ -1,6 +1,7 @@
 -- Default config
 local config = {
-	title = "URLs: ", -- prompt title
+	title = "Links: ", -- prompt title
+	default_picker = "default", -- "default" (vim.ui.select) or "telescope"
 	use_netrw = true, -- use netrw to open urls, if false use xdg-open/open system command
 	debug = true, -- logs user errors
 }
--- lua/urlview/utils.lua
+++ lua/urlview/utils.lua
@@ -2,45 +2,6 @@ local M = {}
 
 local config = require("urlview.config")
 
--- SEE: lua pattern matching (https://riptutorial.com/lua/example/20315/lua-pattern-matching)
--- regex equivalent: [A-Za-z0-9@:%._+~#=/\-?&]*
-local pattern = "[%w@:%%._+~#=/%-?&]*"
-local http_pattern = "https?://"
-local www_pattern = "www%."
-
---- Extracts urls from the given content
----@param content string
----@return table (list) of captures (prefix, url map)
-function M.extract_urls(content)
-	---@type table (set)
-	local captures = {}
-
-	-- Extract URLs starting with http:// or https://
-	for capture in content:gmatch(http_pattern .. "%w" .. pattern) do
-		local prefix = capture:match(http_pattern)
-		local url = capture:gsub(http_pattern, "")
-		captures[url] = prefix
-	end
-
-	-- Extract URLs starting with www, excluding already extracted http(s) URLs
-	for capture in content:gmatch(www_pattern .. "%w" .. pattern) do
-		if not captures[capture] then
-			captures[capture] = ""
-		end
-	end
-
-	-- Combine captures
-	local result = {}
-	for url, prefix in pairs(captures) do
-		local link = prefix .. url
-		if link ~= "" then
-			table.insert(result, link)
-		end
-	end
-
-	return result
-end
-
 --- Opens the url in the browser
 ---@param url string
 function M.navigate_url(url)
@@ -49,9 +10,9 @@ function M.navigate_url(url)
 	else
 		-- supports MacOS, Linux, and FreeBSD
 		local cmd = nil
-		if vim.fn.has "mac" == 1 then -- MacOS
+		if vim.fn.has("mac") == 1 then -- MacOS
 			cmd = "open "
-		elseif vim.fn.has "linux" == 1 or vim.fn.has "bsd" then -- Linux and FreeBSD
+		elseif vim.fn.has("linux") == 1 or vim.fn.has("bsd") then -- Linux and FreeBSD
 			cmd = "xdg-open "
 		end
 
