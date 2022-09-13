eedf84f0f0f41c029d4cef3dcabbb000f5d42648
Andrew <x.and.i.rew.e@gmail.com>
2022-09-13 11:14:30 -0400

    extracted test cases

--- tests/urlview/capture_jump_spec.lua
+++ tests/urlview/capture_jump_spec.lua
@@ -10,7 +10,20 @@ local jump_backwards = jump_helpers.jump_backwards
 
 -- ASSUMPTION(cursor_pos): line numbers are 1-indexed, column numbers are 0-indexed
 
+local examples = {
+  empty = "",
+  invalid = "hello",
+  single_line_middle = "abc https://google.com def",
+  standard_url = "https://www.google.com",
+  multi_line_just_links = [[
+https://www.google.com
+https://www.github.com
+https://www.amazon.com
+https://www.reddit.com]],
+}
+
 describe("line_match_positions unit tests", function()
+  -- TEMP: 1-indexed results
   it("multiple substrings", function()
     local line = "abc abc abc"
     local res = jump.line_match_positions(line, "abc", 0)
@@ -20,22 +33,24 @@ describe("line_match_positions unit tests", function()
   it("single URL", function()
     local url = "https://www.google.com"
     local res = jump.line_match_positions(url, url, 0)
-    assert_tbl_same_ordered({ 1 }, res) -- TEMP: 1-indexed
+    assert_tbl_same_ordered({ 1 }, res)
   end)
 end)
+
+-- TODO: start column correction test
 
 describe("backwards jump", function()
   after_each(teardown_windows)
 
   it("empty buffer", function()
-    local content = ""
+    local content = examples.empty
     create_buffer(content, { 1, 0 })
     local res = jump_backwards()
     assert.is_nil(res)
   end)
 
   it("no URL", function()
-    local content = "hello"
+    local content = examples.invalid
     create_buffer(content)
     for col = 0, #content do
       set_cursor({ 1, col })
@@ -43,6 +58,19 @@ describe("backwards jump", function()
       assert.is_nil(res)
     end
   end)
+
+  -- it("just URL", function()
+  --   local content = examples.standard_url
+  --   create_buffer(content, { 1, 0 })
+  --   local res = jump_backwards()
+  --   assert.is_nil(res)
+  --
+  --   for col = 1, #content do
+  --     set_cursor({ 1, col })
+  --     res = jump_backwards()
+  --     assert_tbl_same_ordered({ 1, 0 }, res)
+  --   end
+  -- end)
 
   it("simple jump start of single line", function()
     local content = "https://www.google.com some random content"
@@ -88,14 +116,24 @@ describe("forwards jump", function()
   after_each(teardown_windows)
 
   it("empty buffer", function()
-    local content = ""
+    local content = examples.empty
     create_buffer(content, { 1, 0 })
     local res = jump_forwards()
     assert.is_nil(res)
   end)
 
   it("no URL", function()
-    local content = "hello"
+    local content = examples.invalid
+    create_buffer(content)
+    for col = 0, #content do
+      set_cursor({ 1, col })
+      local res = jump_forwards()
+      assert.is_nil(res)
+    end
+  end)
+
+  it("just URL", function()
+    local content = examples.standard_url
     create_buffer(content)
     for col = 0, #content do
       set_cursor({ 1, col })
@@ -105,7 +143,7 @@ describe("forwards jump", function()
   end)
 
   it("same line single", function()
-    local content = "abc https://www.google.com def"
+    local content = examples.single_line_middle
     local url_start = 4
     create_buffer(content)
     for col = 0, url_start - 1 do
@@ -116,7 +154,7 @@ describe("forwards jump", function()
   end)
 
   it("on last URL + no more", function()
-    local content = "abc https://www.google.com def"
+    local content = examples.single_line_middle
     local url_start = 4
     create_buffer(content)
     for col = url_start, #content do
@@ -127,12 +165,8 @@ describe("forwards jump", function()
   end)
 
   it("multiline jump from anywhere in line", function()
-    local content = [[
-https://www.google.com
-https://www.github.com
-https://www.amazon.com
-https://www.reddit.com]]
-    local url_length = #"https://www.google.com"
+    local content = examples.multi_line_just_links
+    local url_length = #examples.standard_url
     create_buffer(content)
     -- jumps to next line
     for line = 1, 3 do
