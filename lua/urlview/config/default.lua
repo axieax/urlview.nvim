bdbdf1e020e283551f003e71b0004096c746ef57
Kevin Traver <kevintraver@kevintraver.com>
2023-09-19 09:27:56 -0600

    feat(actino): add option to set clipboard register (#57)

--- lua/urlview/config/default.lua
+++ lua/urlview/config/default.lua
@@ -10,6 +10,9 @@ local default_config = {
   -- Options: "netrw", "system" (default OS browser), "clipboard"; or "firefox", "chromium" etc.
   -- By default, this is "netrw", or "system" if netrw is disabled
   default_action = "netrw",
+  -- Set the register to use when yanking
+  -- Default: + (system clipboard)
+  default_register = "+",
   -- Whether plugin URLs should link to the branch used by your package manager
   default_include_branch = false,
   -- Ensure links shown in the picker are unique (no duplicates)
